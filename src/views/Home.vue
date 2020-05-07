b68df282865ae6082efcb5d84ee251ab5629e8bb
359743984@qq.com <359743984@qq.com>
2020-05-07 17:19:54 +0800

    fix: bug fixed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,15 +9,11 @@
 <script lang="ts">
 import HelloWorld from '@/components/HelloWorld.vue';
 import { state, increment } from '@/states/counter';
-import { getCurrentInstance } from 'vue';
 export default {
   components: {
     HelloWorld
   },
   setup() {
-    // eslint-disable-next-line
-    const { ctx }: any = getCurrentInstance();
-
     return {
       state,
       increment
