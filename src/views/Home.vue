680f5cfbafc08dd95b9f965e691f0e7606da7b0a
359743984@qq.com <359743984@qq.com>
2020-05-07 16:18:02 +0800

    add: 增加了iconfont

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="@/assets/logo.png" @click="increment" />
+    <i class="ft">&#xe663;</i>
+    <br />
     {{ state.counter }}
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
